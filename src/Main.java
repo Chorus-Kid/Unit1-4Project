e4e9e8a9c77b326a3d8a209171a13491df2a8310
Chorus-Kid <145377270+Chorus-Kid@users.noreply.github.com>
2023-11-02 13:58:56 -0400

    Update Main.java
    just need to test more examples just in case something doesn't work

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         Shipping shipName = new Shipping();
+        System.out.println("This generator will generate a couple/ship name for two people you think will be good together!");
+        System.out.println("After that, it will tell you how compatible they are!");
         System.out.print("Enter first person's name here (Must be capitalized): ");
         String firstName = s.nextLine();
         System.out.print("Enter second person's name here (Also must be capitalized): ");
         String secondName = s.nextLine();
-        System.out.print(shipName.shipNameGenerator(firstName, secondName));
+        System.out.println(shipName.shipNameGenerator(firstName, secondName));
+        shipName.compatibilityPercent();
     }
 }
